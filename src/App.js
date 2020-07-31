d6247bcaa54001bdbda8355f495bce2ddb7a215a
Alec Sands <sandsalecv@gmail.com>
2020-07-31 11:32:06 -0500

    refactoring app to use class format and added api call

--- src/App.js
+++ src/App.js
@@ -2,25 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      items: [],
+      error: false
+    }
+  }
+
+  componentDidMount() {
+    let items = fetch('https://still-anchorage-53867.herokuapp.com/items')
+      .then(response => response.json())
+      .then((jsonData) => {
+        this.setState({
+          loading: false,
+          items: jsonData.results
+        });
+      })
+      .catch((error) => {
+        console.error(error.message)
+        this.setState({error: true})
+      });
+
+  }
+
+  render() {
+    const loading = this.state.loading;
+    const items = this.state.items;
+    const error = this.state.error;
+
+    if (loading) {
+      return (<p>Loading...</p>)
+    } else if (error) {
+      return (<p>There was an error</p>)
+    } else {
+      return (<div>{items.map((d,i) => {return <p key={i}>{d.name}</p>})}</div>)
+    }
+  }
 }
 
 export default App;
